06201748c5dd18b105e52683e96a84ef0bcb0610
Amirkhan Orazbay <amirhan9878@gmail.com>
2023-09-25 21:00:22 +0000

    fgrsefaegsr

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+const https = require('https');
+  
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if (req.query.account == 'id4' || req.query.account == 'id5') {
@@ -9,6 +10,20 @@ router.get('/', function(req, res, next) {
     } else {
       res.redirect(`https://oldpayments.ala-laundry.com/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`);
     }
+    const request = https.request(`https://oldpayments.ala-laundry.com/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`, (response) => {
+      let data = '';
+      response.on('data', (chunk) => {
+        data = data + chunk.toString();
+      });
+
+      response.on('end', () => {
+        const body = JSON.parse(data);
+        console.log(body);
+      });
+    })
+
+
+    request.end() 
   } else {
     if (req.query.command == 'check') {
       res.redirect(`https://newpayments.ala-laundry.com/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`);
