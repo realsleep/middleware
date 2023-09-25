95604bb76b0ac314c7aba3d6e4bd0c9fbf874ebb
Amirkhan Orazbay <amirhan9878@gmail.com>
2023-09-25 21:18:20 +0000

    dhtgrsfeagsrdhtgrs

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,31 @@ const https = require('https');
   
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let options = {
+    port: 443,
+    method: 'GET',
+    key: fs.readFileSync('../key.pem'),
+    cert: fs.readFileSync('../cert.pem'),
+  };
+
   if (req.query.account == 'id4' || req.query.account == 'id5') {
+    options['hostname'] = 'oldpayments.ala-laundry.com'
     if (req.query.command == 'check') {
-      res.redirect(`https://oldpayments.ala-laundry.com/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`);
+      options['path'] = `/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`;            
     } else {
-      res.redirect(`https://oldpayments.ala-laundry.com/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`);
+      options['path'] = `/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`;
     }
-    const request = https.request(`https://oldpayments.ala-laundry.com/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`, (response) => {
+  } else {
+    options['hostname'] = 'newpayments.ala-laundry.com';
+    if (req.query.command == 'check') {
+      options['path'] = `/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`;
+    } else {
+      options['path'] = `/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`;
+    }
+  }
+
+  options.agent = new https.Agent(options);
+  const request = https.request(options, (response) => {
       let data = '';
       response.on('data', (chunk) => {
         data = data + chunk.toString();
@@ -20,20 +38,13 @@ router.get('/', function(req, res, next) {
         const body = JSON.parse(data);
         console.log(body);
       });
-    })
+  });
 
-    request.on('error', (error) => {
-      console.log('An error', error);
-    });
+  request.on('error', (error) => {
+    console.log('An error', error);
+  });
 
-    request.end() 
-  } else {
-    if (req.query.command == 'check') {
-      res.redirect(`https://newpayments.ala-laundry.com/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`);
-    } else {
-      res.redirect(`https://newpayments.ala-laundry.com/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`);
-    }
-  }
+  request.end();
 });
 
 module.exports = router;
