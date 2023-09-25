b715016f36f3e4a63a2f7ff55c0d4a0c7d3b8958
Amirkhan Orazbay <amirhan9878@gmail.com>
2023-09-25 20:51:28 +0000

    awfegrseafef

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if (req.query.account == 'id4' && req.query.account == 'id5') {
-    res.redirect('https://oldpayments.ala-laundry.com');
+    if (req.query.command == 'check') {
+      res.redirect(`https://oldpayments.ala-laundry.com/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`);
+    } else {
+      res.redirect(`https://oldpayments.ala-laundry.com/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`);
+    }
   } else {
-    res.redirect('https://newpayments.ala-laundry.com');
+    res.redirect(`https://newpayments.ala-laundry.com`);
   }
 });
 
