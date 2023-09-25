a8884e908ae72d0492621122bfc20685d5bfa30d
Amirkhan Orazbay <amirhan9878@gmail.com>
2023-09-25 20:54:14 +0000

    gsrfaedwfsgrf

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if (req.query.account == 'id4' && req.query.account == 'id5') {
+  if (req.query.account == 'id4' || req.query.account == 'id5') {
     if (req.query.command == 'check') {
       res.redirect(`https://oldpayments.ala-laundry.com/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`);
     } else {
