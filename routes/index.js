b9257c30ca200f5de08ecca06f4281f5323ee8b4
Amirkhan Orazbay <amirhan9878@gmail.com>
2023-09-25 22:10:44 +0000

    afegsrhdtsgfaegrhd

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
   
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/payment', function(req, res, next) {
   let options = {
     port: 80,
     method: 'GET',
@@ -16,16 +16,49 @@ router.get('/', function(req, res, next) {
     if (req.query.command == 'check') {
       options['path'] = `/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`;            
     } else {
-      options['path'] = `/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`;
+      options['path'] = `/kaspi/payment?command=pay&txn_id=${req.query.txn_id}&account=${req.query.account}`;
     }
   } else {
     options['hostname'] = 'newpayments.ala-laundry.com';
     if (req.query.command == 'check') {
       options['path'] = `/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`;
     } else {
-      options['path'] = `/kaspi/payment?command=check&txn_id=${req.query.txn_id}&account=${req.query.account}`;
+      options['path'] = `/kaspi/payment?command=pay&txn_id=${req.query.txn_id}&account=${req.query.account}`;
     }
   }
+
+  const request = https.request(options, (response) => {
+      let data = '';
+      response.on('data', (chunk) => {
+        data = data + chunk.toString();
+      });
+
+      response.on('end', () => {
+        const body = JSON.parse(data);
+        res.json(body);
+      });
+  });
+
+  request.on('error', (error) => {
+    console.log('An error', error);
+  });
+
+  request.end();
+});
+
+router.get('/get_price', function(req, res, next) {
+  let options = {
+    port: 80,
+    method: 'GET',
+  };
+
+  if (req.query.account == 'id4' || req.query.account == 'id5') {
+    options['hostname'] = 'oldpayments.ala-laundry.com'
+  } else {
+    options['hostname'] = 'newpayments.ala-laundry.com';
+  }
+
+  options['path'] = `/kaspi/get_price?service_id=${req.query.service_id}`;
 
   const request = https.request(options, (response) => {
       let data = '';
